dcba52635ef22260d6f858ccc7cf5f628c0bbff4
Jan Eckerlein <jan.eckerlein@fkt42.de>
2023-03-06 23:18:30 +0100

    fix db setup

--- php/src/index.php
+++ php/src/index.php
@@ -1,25 +1,33 @@
 <?php
-echo "hello world";
-
 //These are the defined authentication environment in the db service
 
 // The MySQL service named in the docker-compose.yml.
 $host = 'db';
-
-// Database use name
-$user = 'MYSQL_USER';
-
-//database user password
-$pass = 'MYSQL_PASSWORD';
+$pass = getenv('MYSQL_ROOT_PASSWORD');
+$db = getenv('MYSQL_DATABASE');
 
 
 // check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
+$conn = new mysqli($host, 'root', $pass, getenv('MYSQL_DATABASE'));
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
+  die("Connection failed: " . $conn->connect_error);
 }
 
+// var_dump($conn);
+// select query
+$sql = 'SELECT * FROM users';
+$users = array();
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
+}
 
 ?>
